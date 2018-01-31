3d1bfee3e3777c20bbd99ab88cd5a091bfe1f4be
tungbt1 <tungbuivn@gmail.com>
2018-01-31 09:15:49 +0700

    .

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 let express = require("express");
 let request = require('request');
 let cheerio = require('cheerio');
@@ -6,73 +5,74 @@ let cheerio = require('cheerio');
 let fs = require("fs");
 let app = require('express')();
 let http = require('http').Server(app);
-let path=require('path');
+let path = require('path');
 let clientListNames = [];
 let prod = true;
-let root=path.resolve(__dirname+"/../data");
-const comm=require('./common');
+let root = path.resolve(__dirname + "/../data");
+const comm = require('./common');
 // var bodyParser = require('body-parser');
-const sites=comm.sites;
-const getHtml=comm.getHtml;
+const sites = comm.sites;
+const getHtml = comm.getHtml;
 
-app.use(express.static(root+'/../exports'));
+app.use(express.static(root + '/../exports'));
 app.use(express.static('fonts'));
 app.use(express.static('dist'));
 app.use(express.static('server'));
 app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+app.use(express.urlencoded({extended: true})); // to support URL-encoded bodies
 
 
 // app.use(bodyParser.json());
-app.post('/save', function(req, res) {
+app.post('/save', function (req, res) {
 
-    var id = parseInt(req.body.id)+1000,
+    var id = parseInt(req.body.id) + 1000,
         url = req.body.url,
-        name=req.body.name;
-    var lst=[];
-    if (fs.existsSync(root+"/read.json")) {
-        lst=JSON.parse( fs.readFileSync(root+"/read.json")+"");
+        name = req.body.name;
+    var lst = [];
+    if (fs.existsSync(root + "/read.json")) {
+        lst = JSON.parse(fs.readFileSync(root + "/read.json") + "");
     }
-    var fo=lst.filter(o=>o.name==name);
+    var fo = lst.filter(o => o.name == name);
     if (fo.length) {
-        fo[0].url=url;
+        fo[0].url = url;
     } else {
-        lst.push({id,url,name,reading:true});
+        lst.push({id, url, name, reading: true});
     }
-    lst=lst.map((o,idx)=>{
-        return Object.assign({},o,{id:idx+1000});
+    lst = lst.map((o, idx) => {
+        return Object.assign({}, o, {id: idx + 1000});
 
     });
-    fs.writeFileSync(root+"/read.json",JSON.stringify(lst));
-    res.json({status:0})
+    fs.writeFileSync(root + "/read.json", JSON.stringify(lst));
+    res.json({status: 0})
 
 });
-function writeHtml(res,html){
-  var s=fs.readFileSync(root+'/../src/tpl.html')+"";
-  s=s.replace("xxxxx",html);
-  res.send(s);
+
+function writeHtml(res, html) {
+    var s = fs.readFileSync(root + '/../src/tpl.html') + "";
+    s = s.replace("xxxxx", html);
+    res.send(s);
 }
-app.get('/s/:id',(req,res)=>{
-  var cfg=JSON.parse(fs.readFileSync(root+'/../server/story.json'));
-  var chapters=JSON.parse(fs.readFileSync(root+'/'+cfg.dir+"/chapters.json")+"");
 
-  if (req.params.id=="0"){
-    var lst=chapters;
-    var i=0;
-    writeHtml(res,lst.map(o=>"<p><a href='/s/"+(++i)+"'>"+o.title+"</a></p>").join(""));
-} else {
-    var id=parseInt(req.params.id)-1;
-    var j=id+2;
-    if (j>chapters.length) j=chapters.length;
-    var site=require('./local');
-    var url=chapters[id].link;
-    site.documentContent(cfg.dir,url).then((rs) => {
+app.get('/s/:id', (req, res) => {
+    var cfg = JSON.parse(fs.readFileSync(root + '/../server/story.json'));
+    var chapters = JSON.parse(fs.readFileSync(root + '/' + cfg.dir + "/chapters.json") + "");
 
-      writeHtml(res,"<h4><a href='/s/"+(j)+"'>"+rs.title+"</a></h4><p>"+rs.data.join("</p><p>")+"</p>");
-    }).catch(comm.error);
+    if (req.params.id == "0") {
+        var lst = chapters;
+        var i = 0;
+        writeHtml(res, lst.map(o => "<p><a href='/s/" + (++i) + "'>" + o.title + "</a></p>").join(""));
+    } else {
+        var id = parseInt(req.params.id) - 1;
+        var j = id + 2;
+        if (j > chapters.length) j = chapters.length;
+        var site = require('./local');
+        var url = chapters[id].link;
+        site.documentContent(cfg.dir, url).then((rs) => {
+            writeHtml(res, "<h4><a href='/s/" + (j) + "'>" + rs.title + "</a></h4><p>" + rs.data.join("</p><p>") + "</p>");
+        }).catch(comm.error);
 
 //writeHtml(res,id);
-}
+    }
 });
 // app.get('/doc-epub/:name/:url',(req,res)=>{
 //     var siteId=0;
@@ -95,18 +95,18 @@ app.get('/s/:id',(req,res)=>{
 //         }).catch(comm.error)
 //     }
 // });
-app.get('/doc-content/:siteid/:name/:url',(req,res)=>{
+app.get('/doc-content/:siteid/:name/:url', (req, res) => {
     // res.writeHead(200, {'Content-Type': 'application/json'});
-    let fo=sites.filter(o=>o.id==req.params.siteid);
+    let fo = sites.filter(o => o.id == req.params.siteid);
     if (fo.length) {
         let site = require("./" + fo[0].name);
 
 
-        let url=req.params.url.split("-").map(o => String.fromCharCode(parseInt(o, 16))).join("");
-        let name=req.params.name.split("-").map(o => String.fromCharCode(parseInt(o, 16))).join("");
-        if (req.params.siteid==0) {
+        let url = req.params.url.split("-").map(o => String.fromCharCode(parseInt(o, 16))).join("");
+        let name = req.params.name.split("-").map(o => String.fromCharCode(parseInt(o, 16))).join("");
+        if (req.params.siteid == 0) {
 
-            site.documentContent(name,url).then((rs) => res.json(rs)).catch(comm.error);
+            site.documentContent(name, url).then((rs) => res.json(rs)).catch(comm.error);
             return;
         }
 
@@ -117,16 +117,16 @@ app.get('/doc-content/:siteid/:name/:url',(req,res)=>{
 });
 
 
-app.get('/doc-list/:siteid/:url',(req,res)=>{
+app.get('/doc-list/:siteid/:url', (req, res) => {
     // res.writeHead(200, {'Content-Type': 'application/json'});
-    let fo=sites.filter(o=>o.id==req.params.siteid);
+    let fo = sites.filter(o => o.id == req.params.siteid);
     if (fo.length) {
         let site = require("./" + fo[0].name);
 
 
-        let url=req.params.url.split("-").map(o => String.fromCharCode(parseInt(o, 16))).join("");
-        if (req.params.siteid==0) {
-            site.documentList(url).then(rs=>res.json(rs)).catch(comm.error);
+        let url = req.params.url.split("-").map(o => String.fromCharCode(parseInt(o, 16))).join("");
+        if (req.params.siteid == 0) {
+            site.documentList(url).then(rs => res.json(rs)).catch(comm.error);
             return;
         }
 
@@ -148,21 +148,21 @@ app.get('/doc-list/:siteid/:url',(req,res)=>{
     }
 });
 
-app.get('/site-detail/:id',(req,res)=>{
+app.get('/site-detail/:id', (req, res) => {
 
 
     // res.writeHead(200, {'Content-Type': 'application/json'});
-    let fo=sites.filter(o=>o.id==req.params.id);
+    let fo = sites.filter(o => o.id == req.params.id);
     if (fo.length) {
-        let site=require("./"+fo[0].name);
-        if (req.params.id==0) {
-            site.siteListItem().then(rs=>{
+        let site = require("./" + fo[0].name);
+        if (req.params.id == 0) {
+            site.siteListItem().then(rs => {
                 res.send(rs);
             });
             return;
         }
-        getHtml(fo[0].url).then((data)=>{
-            site.siteListItem(data).then((rs)=>res.json(rs)).catch(comm.error);
+        getHtml(fo[0].url).then((data) => {
+            site.siteListItem(data).then((rs) => res.json(rs)).catch(comm.error);
 
             // let $=cheerio.load(data);
             // let list=[];
@@ -179,11 +179,11 @@ app.get('/site-detail/:id',(req,res)=>{
     }
 });
 
-app.get('/sites',(req,res)=>{
+app.get('/sites', (req, res) => {
     // res.writeHead(200, { 'Cache-Control': 'no-cache'});
-    var lst=[];
-    if (fs.existsSync(root+"/read.json")) {
-        lst=JSON.parse( fs.readFileSync(root+"/read.json")+"");
+    var lst = [];
+    if (fs.existsSync(root + "/read.json")) {
+        lst = JSON.parse(fs.readFileSync(root + "/read.json") + "");
     }
     res.json([].concat(sites).concat(lst));
 
